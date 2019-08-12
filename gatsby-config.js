ad949b98c2e72c915946672c98bb819f99eaf98a
Aivan Monceller <aivan@monceller.com>
2019-08-12 20:00:45 +0800

    chore(init): initial gatsby commit

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: '@geocine/gatsby-theme-organization',
+      options: {
+        contentPath: 'data',
+        organization: 'EDSA',
+        url: 'http://edsa.io/',
+        title: 'Empowering Developers of Software Applications',
+        iconName: 'icon.png'
+      }
+    }
+  ]
+}
